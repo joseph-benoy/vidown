49d94fb9bc87c2e2e4774dd89dd0e0dc9c5ce24d
Joseph Benoy <josephbenoy03@gmail.com>
2021-06-15 01:51:03 +0530

    added get video quality and audio quality methods

---
+++ index.js
@@ -0,0 +1,23 @@
+const yt = require('./yt');
+const fs = require('fs');
+
+/*const download = async ()=>{
+    try{
+        const options = {quality:'highestaudio'};
+        const info = await ytdl.getInfo("https://www.youtube.com/watch?v=aqz-KE-bpKQ");
+        const audioFormats = ytdl.filterFormats(info.formats,'videoandaudio');
+        for(let i of info.formats){
+            if(i['hasVideo']==false){
+                console.log(i['qualityLabel']+" : "+i['container']);
+            }
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+download();*/
+(async ()=>{
+    console.log(await yt.getVideoQaulities("https://www.youtube.com/watch?v=s-bZD3O3P80"));
+})();
+
---
+++ yt.js
@@ -0,0 +1,38 @@
+let ytdl = require('ytdl-core');
+
+const yt = {
+    getVideoQaulities:async (url)=>{
+        try{
+            const info = await ytdl.getInfo(url);
+            const videoFormats = ytdl.filterFormats(info.formats,'video');
+            let qualityList = {};
+            for(let i of videoFormats){
+                if(i['hasVideo']==true){
+                    qualityList[i['bitrate']] = i['container'];
+                }
+            }
+            return qualityList;
+        }
+        catch(err){
+            console.log(err);
+        }
+    },
+    getAudioQualities:async (url)=>{
+        try{
+            const info = await ytdl.getInfo(url);
+            const videoFormats = ytdl.filterFormats(info.formats,'audioonly');
+            let qualityList = {};
+            for(let i of videoFormats){
+                if(i['hasVideo']==false){
+                    qualityList[i['bitrate']] = i['container'];
+                }
+            }
+            return qualityList;
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+};
+
+module.exports = yt;
