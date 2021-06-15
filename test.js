d8e0c7489eea96097c949b65860568c7cfde8396
Joseph Benoy <josephbenoy03@gmail.com>
2021-06-16 01:54:34 +0530

    removed test

--- test.js
+++
@@ -1,4 +0,0 @@
-var str = "സ്‌കേറ്റര്‍ ഗേള്‍: സ്വാതന്ത്ര്യത്തിന്റെ ലോകത്ത് പറക്കാന്‍ ആഗ്രഹിക്കുന്നവരുടെ കഥ";
-var combining = /[\u0300-\u036F]/g; 
-
-console.log(str.normalize('NFKD').replace(combining, ''));
